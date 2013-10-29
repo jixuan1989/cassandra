2a8393ef647cfef60b9056154776781987074414
EricPai <EricPai@ericpai-pc>
2013-10-29 08:54:11 +0800

    Add comment to Staleness Logger

--- src/java/cn/edu/thu/thss/log/StalenessLogger.java
+++ src/java/cn/edu/thu/thss/log/StalenessLogger.java
@@ -10,9 +10,13 @@ import org.apache.cassandra.utils.ByteBufferUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 记录写操作流程中时间点的日志类
+ * @author EricPai
+ */
 public class StalenessLogger {
 	private static final Logger logger = LoggerFactory.getLogger(StalenessLogger.class);
-	private static final String TEST_KEYSPACE = "test_metrics";
+	private static final String TEST_KEYSPACE = "test_metrics";//要记录日志的KS的前缀
 	
 	public static final String CDR_NODE_ENQUEUE = "coordinatorNodeEnqueue";
 	public static final String CDR_NODE_SEND = "coordinatorNodeSend";
@@ -24,6 +28,15 @@ public class StalenessLogger {
 	public static final String CDR_NODE_APPLY_START = "coordinatorNodeApplyStart";
 	public static final String CDR_NODE_APPLY_FINISH = "coordinatorNodeApplyFinish";
 	
+	/**
+	 * 记录当本节点为协调者节点时MessageOut的重要时间点
+	 * @param mo MessageOut对象
+	 * @param mid MessageOut的Id
+	 * @param timeStamp 时间点
+	 * @param dest 目的IP地址
+	 * @param timeType 时间点类型，包括入队时间<code>StalenessLogger.<i>CDR_NODE_ENQUEUE</i></code>
+	 * 			和发送完成时间<code>StalenessLogger.<i>CDR_NODE_SEND</i></code>
+	 */
 	public static void messageOutToLog(MessageOut mo, String mid, long timeStamp, InetAddress dest, String timeType) {
 		if (mo.verb == Verb.MUTATION && 
 			mo.payload instanceof RowMutation && 
@@ -42,6 +55,16 @@ public class StalenessLogger {
 		}
 	}
 	
+	/**
+	 * 记录当本节点为非协调者节点时MessageIn的重要时间点
+	 * @param mi MessageIn对象
+	 * @param mid MessageIn的Id
+	 * @param timeStamp 时间点
+	 * @param timeType 时间点类型，包括开始接收时间<code>StalenessLogger.<i>SUB_NODE_RECEIVE</i></code>,
+	 * 			处理队列入队开始时间<code>StalenessLogger.<i>SUB_NODE_ENQUEUE</i></code>,
+	 * 			处理开始时间<code>StalenessLogger.<i>SUB_NODE_APPLY</i></code>,
+	 * 			和处理完成时间<code>StalenessLogger.<i>SUB_NODE_FINISH</i></code>
+	 */
 	public static void messageInToLog(MessageIn mi, String mid, long timeStamp, String timeType) {
 		if (mi.verb == Verb.MUTATION && 
 			mi.payload instanceof RowMutation && 
@@ -59,6 +82,15 @@ public class StalenessLogger {
 		}
 	}
 	
+	/**
+	 * 记录当本节点为协调者节点时本地写操作执行的重要时间点
+	 * @param rm RowMutation对象
+	 * @param timeStamp 时间点
+	 * @param localAddr 本地IP地址
+	 * @param timeType 时间点类型，包括处理队列入队开始时间<code>StalenessLogger.<i>CDR_NODE_APPLY_ENQUEUE</i></code>,
+	 * 			处理开始时间<code>StalenessLogger.<i>CDR_NODE_APPLY_START</i></code>,
+	 * 			和处理完成时间<code>StalenessLogger.<i>CDR_NODE_APPLY_FINISH</i></code>
+	 */
 	public static void coordinatorLocalApplyToLog(RowMutation rm, long timeStamp, InetAddress localAddr, String timeType) {
 		if (rm.getTable().startsWith(TEST_KEYSPACE)) {
 				StringBuffer sb = new StringBuffer();
@@ -73,6 +105,10 @@ public class StalenessLogger {
 		}
 	}
 	
+	/**
+	 * 得到当前的时间点
+	 * @return long类型表示的时间点,目前为纳秒级别
+	 */
 	public static long getCurrentTime() {
 		return System.nanoTime();
 	}
